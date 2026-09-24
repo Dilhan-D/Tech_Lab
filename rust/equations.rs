23c5baffab1458c2e0a1178285b7e876a2bed10c
Dilhan D <Dilhan.dincer@epitech.eu>
2026-09-24 14:54:41 +0200

    And,or, not invert boolean value

--- rust/equations.rs
+++ rust/equations.rs
@@ -28,4 +28,13 @@ fn main() {
  // &&	AND	true if both values are true
  // ||	OR	true if at least one is true
  // !	NOT	inverts the boolean value
+
+  let logged_in = true;
+  let is_admin = false;
+
+  println!("Is regular user: {}", logged_in && !is_admin);
+  println!("Has any access: {}", logged_in || is_admin);
+  println!("Not logged in: {}", !logged_in);
 }
+
+
